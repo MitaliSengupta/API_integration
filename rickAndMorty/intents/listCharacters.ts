8e6cd2fc9d702df2e331d2a57aec2cb1f4d6f47d
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-13 21:33:00 +0000

    getting random number generated

--- rickAndMorty/intents/listCharacters.ts
+++ rickAndMorty/intents/listCharacters.ts
@@ -2,15 +2,12 @@ import { FetchData, TnoAuthContext, TFetchDataCallback } from '@bearer/intents'
 // Uncomment this line if you need to use Client
 import Client from './client'
 
-
-
 export default class ListCharactersIntent {
   static intentName: string = 'listCharacters'
   static intentType: any = FetchData
 
-  
-  static action(context: TnoAuthContext, params: any, body: any, callback: TFetchDataCallback) {
-    Client().get('character/1')
+  static action(context: TnoAuthContext, params: any, body: any, callback: TFetchDataCallback) {    
+    Client().get('character/' + (Math.floor(Math.random() * (493 - 1 + 1)) + 1).toString() + '/')
 	.then(({ data }) => {
     		 callback({ data })
 	      })
